acbe9db324885a15a954b71038d6d05f64b8afd2
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2024-07-19 02:07:39 -0400

    added some mobile responsiveness, fixed homepage positioning for laptop sized screens.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,17 @@
 //a description of how im pursuing software development, how long, why dev, my name, where im from.
 import React from 'react'
-
+import './About.css'
 export default function About() {
   return (
     <div >
       <h1>About Me</h1>
-      <p className='About-Paragraph'>At 22 years old, hailing from a small town in Vermont, I ventured into programming initially drawn by the lucrative prospects it offers. Over time, my journey has been enriched by a deep-seated passion for computers—an admiration stemming from their complexity and transformative power. Beyond coding, I find joy in snowboarding, relishing the thrill of gliding down slopes, and cherish moments spent with friends, whose company adds vibrancy to my life.</p>
+      <p className='About-Paragraph'>At 22 years old, hailing from a small town in Vermont, 
+      I ventured into programming initially drawn by the lucrative prospects it offers. Over time, my journey has been enriched 
+      by a deep-seated passion for computers—an admiration stemming from their complexity and transformative power. Beyond coding, 
+      I find joy in snowboarding, relishing the thrill of gliding down slopes, and cherish moments spent with friends, 
+      whose company adds vibrancy to my life. I want to do my entire part, with providing software to the community, and or working on large scale repos.
+      Some goals I feel like I should acomplish is acomplish more training, plus take certain programs like OWASP, and certain courses that teach python, java, and or type-script.
+      I also think its important to learn a different stack, so possiblly pursure learning knowledge with angular.</p>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,8 +25,15 @@ export default function Home() {
    
   <div class='text-overlay'>Hello, im Alex Aubin Im intensively pursuing my <strong>software development certificate</strong> at, Upright Education</div>
   </div>
+  <div className="inspirational-paragraph">
+Ive recently finished my program, and with the skills I learned at BCA Im happy to announce I will be developing software as a passion.
+If you have an idea for a software application, but arent quite sure how to make it work come to me and Ill be your best solution.
+Theirs an art to making full stack web applications, additonally a variety of qualification/skills implemented to achieve a succesfull project
+  </div>
   <HomeFlexBox/>
-
+<div  className="typing-animation">
+  im excited to work on other projects
+</div>
  </>
   )
 
