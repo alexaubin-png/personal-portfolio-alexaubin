53632eb85b97db47c9017b421c83ad2e52d7a4ee
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2024-08-12 23:00:16 -0400

    fixed hiremebutton width for mobile devices TODO: fix hobbies page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,8 +29,10 @@ export default function Home() {
   return (
   
     <div style={currentModeStyles}>
-      <img></img>
-      <p>Hello im Alex Aubin, and im a certified software developer</p>          
+      <h1 className='inspiration-header'>Hello im Alex Aubin, and im a certified software developer</h1>       
+      <div className="inspirational-paragraph">
+        I've recently finished my program, and with the skills I learned at BCA, I'm happy to announce I will be developing software as a passion. If you have an idea for a software application but aren't quite sure how to make it work, come to me and I'll be your best solution. There's an art to making full stack web applications, additionally a variety of qualifications/skills implemented to achieve a successful project. A little less than 30 days prior to obtaining my certificate in Java Script Foundations, I couldnt be more excited to start applying and eventually take up a job as a software developer.
+      </div>   
       <Profile />
       </div>
   );
