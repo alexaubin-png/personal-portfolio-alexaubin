9b6d5b1c9b0c3e0a681c213a206f10d21c55b3be
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2024-09-04 17:55:51 -0400

    hopefuly connected the frontend and backend via env variables, added mobile responsiveness

--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -5,18 +5,54 @@ export default function Websites() {
     //a mobile friendly(full screen highresolution clickable images)
     // that link to deployed projects(projects listed in socail nav file)
     <>
-       <div className='gallery-container'>
-            <div className="img-container">
-              <img className='galleryhero-image-1' src='#' alt=''></img>
-            </div>
-            <div className="img-section">
-              <img className='gallery-image-1' src='#' alt=''></img>
-              <img className='gallery-image-2' src='#' alt=''></img>
-              <img className='gallery-image-3' src='#' alt=''></img>
-              <img className='gallery-image-4' src='#' alt=''></img>
-              <img className='gallery-image-5' src='#' alt=''></img>
-            </div>
-          </div>
+    <section className='slider-container'>
+    <div className="slider-images">
+      <div className="slider-image">
+        <img className='image1' src='./src/assets/AKMP4147.PNG' alt='1'></img>
+        <h1>Capstone</h1>
+        <div className="details">
+        <h2>Indygo</h2>
+          <p>bus stops and map navigation in indianappolis marion county</p>
+  
+        </div>
+      </div>
+      <div  className="slider-image">
+        <img  className='image2' src='./src/assets/AKMP4147.PNG' alt='2'></img>
+        <h1>DevZone</h1>
+        <div className="details">
+        <h2>Personal</h2>
+          <p>a website to teach motivated students how to code as rookie</p>
+  
+        </div>
+      </div>
+      <div className="slider-image">
+        <img   className='image3'src='./src/assets/AKMP4147.PNG' alt='3'></img>
+        <h1>React Chat</h1>
+        <div className="details">
+        <h2>Personal</h2>
+          <p>A website that allows a user to switch rooms + send messages</p>
+  
+        </div>
+      </div>
+      <div className="slider-image">
+        <img  className='image4' src='./src/assets/AKMP4147.PNG' alt='4'></img>
+        <h1>FreeLancing</h1>
+        <div className="details">
+        <h2>Personal</h2>
+          <p>A website that allows you request a website made by me</p>
+  
+        </div>
+      </div>
+      <div className="slider-image">
+        <img  className='image5' src='./src/assets/AKMP4147.PNG' alt='5'></img>
+        <h1>NA</h1>
+        <div className="details">
+          <h2>NA</h2>
+          <p>nothing yet, stay tuned for more content, im motivated as ever</p>
+        </div>
+      </div>
+    </div>
+    </section>
      </>
    
     
